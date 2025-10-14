1cdef25a8dd672c761b208897412041546dfcd31
Lorenzo Brizolla <lorenzobrizolla789@gmail.com>
2025-10-14 12:44:28 -0300

    backup router

--- src/components/DadosProduto.jsx
+++ src/components/DadosProduto.jsx
@@ -1,22 +1,54 @@
 import PlantImage from "../assets/Plant.png";
 import CowImage from "../assets/Cow.png";
-import dados from '../../db.json';
+import dados from "../../db.json";
+import NumberInput from "./NumberInput";
+import Button from "./Button";
 
 export default function DadosProduto() {
+  const { produtos } = dados;
+  function formatarPreco(precoCentavos) {
+    return (precoCentavos / 100).toFixed(2).replace(".", ",");
+  }
   return (
     <>
       <div className="product__container--image">
         <img src="" className="product__image" alt="" />
       </div>
       <div className="product__data">
-        <h1 className="product__title"></h1>
-        <h2 className="product__price"></h2>
-        <div className="product__tag">
-          <img src={PlantImage} alt="planta" />
-          <span>Vegano</span>
-        </div>
-
-        <p className="product__description"></p>
+        <h1 className="product__title">{produtos.nome}</h1>
+        <h2 className="product__price">
+          R$ {formatarPreco(produtos.preco.por)}
+        </h2>
+        {produtos.vegano === true ? (
+          <div className="product__tag">
+            <img src={PlantImage} alt="planta" />
+            <span>Vegano</span>
+          </div>
+        ) : (
+          <div className="product__tag">
+            <img src={CowImage} alt="leite" />
+            <span>Contém Lactose</span>
+          </div>
+        )}
+        ;<p className="product__description">{produtos.descricao}</p>
+        <form>
+          <section className="product__observation">
+            <label htmlFor="observation">Observações sobre o pedido</label>
+            <textarea
+              rows={3}
+              name="observation"
+              id="observation"
+              placeholder="Digite suas observações. Ex.: Enviar açúcar"
+            ></textarea>
+          </section>
+          <div className="product__buy">
+            <NumberInput
+              initial={1}
+              onChange={(qtd) => console.log("Qtd produto:", qtd)}
+            />
+            <Button />
+          </div>
+        </form>
       </div>
     </>
   );
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,19 +2,18 @@ import { useState } from "react";
 import PlantImage from "../assets/Plant.png";
 import CowImage from "../assets/Cow.png";
 import "../css/Product.css";
-import Button from "../components/Button";
-import NumberInput from "../components/NumberInput"; // importa o componente
-/* import { useParams } from "react-router-dom"; */
+import { useParams } from "react-router-dom";
+import DadosProduto from "../components/DadosProduto";
 
-export function Product({ productData }) {
+export function Product() {
   const [quantidade, setQuantidade] = useState(1);
 
   function formatarPreco(precoCentavos) {
     return (precoCentavos / 100).toFixed(2).replace(".", ",");
   }
 
-  /*   const { id } = useParams();
-  console.log('ID do produto:', id); */
+  const { id } = useParams();
+  console.log("ID do produto:", id);
   return (
     <main>
       <div className="container product__container">
@@ -23,56 +22,7 @@ export function Product({ productData }) {
         </a>
 
         <section className="product">
-          {/* componente DadosProduto */}
-          <div className="product__container--image">
-            <img
-              src={productData.imagem}
-              className="product__image"
-              alt="produto 1"
-            />
-          </div>
-
-          <div className="product__data">
-            <h1 className="product__title">{productData.nome}</h1>
-            <h2 className="product__price">
-              R$ {formatarPreco(productData.preco.por)}
-            </h2>
-
-            {productData.vegano === true ? (
-              <div className="product-tag">
-                <img src={PlantImage} alt="planta" />
-                <span>Vegano</span>
-              </div>
-            ) : (
-              <div className="product-tag">
-                <img src={CowImage} alt="leite" />
-                <span>Contém Lactose</span>
-              </div>
-            )}
-            <p className="product__description">
-              {productData.descricao}
-            </p>
-
-            <form>
-              <section className="product__observation">
-                <label htmlFor="observation">Observações sobre o pedido</label>
-                <textarea
-                  rows={3}
-                  name="observation"
-                  id="observation"
-                  placeholder="Digite suas observações. Ex.: Enviar açúcar"
-                ></textarea>
-              </section>
-
-              <div className="product__buy">
-                <NumberInput
-                  initial={1}
-                  onChange={(qtd) => console.log("Qtd produto:", qtd)}
-                />
-                <Button />
-              </div>
-            </form>
-          </div>
+          <DadosProduto />
         </section>
       </div>
     </main>
